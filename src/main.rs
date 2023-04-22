5d1329641543866469c2a0f298bcc606add4278f
theFakeFrog <da_skibbles@proton.me>
2023-04-21 20:53:29 -0400

    first commit of butter os!

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,15 @@ use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-static HELLO: &[u8] = b"hello world!";
+//static HELLO: &[u8] = b"hello world!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("this is println!!");
 
     loop{}
 }
