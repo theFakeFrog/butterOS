c6d9f65da8bd98d23308be66cacb18e8e953113b
theFakeFrog <da_skibbles@proton.me>
2023-04-21 23:45:56 -0400

    added support for testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-// src/main.rs
-
 #![no_std]
 #![no_main]
-
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -19,6 +19,17 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("this is println!!");
 //    panic!("kernal panic"); //example kernal panic for memory
+#[cfg(test)]
+test_main();
 
     loop{}
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
